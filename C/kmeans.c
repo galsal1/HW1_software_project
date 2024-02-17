bb882e4d98ff369ef6372e05c2a3170cfb06c2c8
galsal1 <galsalman@mail.tau.ac.il>
2024-02-17 17:15:54 +0200

    update

--- C/kmeans.c
+++ C/kmeans.c
@@ -1,66 +1,15 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 
 int K,N,d,iter=200;
-char* path;
+FILE * file;
 double* data;
 int* places;
 int* Group_size;
 double* K_centroid;
 double* Mean_vector;
-
-size_t getline(char **lineptr, size_t *n, FILE *stream) {
-    char *bufptr = NULL;
-    char *p = bufptr;
-    size_t size;
-    int c;
-
-    if (lineptr == NULL) {
-        return -1;
-    }
-    if (stream == NULL) {
-        return -1;
-    }
-    if (n == NULL) {
-        return -1;
-    }
-    bufptr = *lineptr;
-    size = *n;
-
-    c = fgetc(stream);
-    if (c == EOF) {
-        return -1;
-    }
-    if (bufptr == NULL) {
-        bufptr = malloc(128);
-        if (bufptr == NULL) {
-            return -1;
-        }
-        size = 128;
-    }
-    p = bufptr;
-    while(c != EOF) {
-        if ((p - bufptr) > (size - 1)) {
-            size = size + 128;
-            bufptr = realloc(bufptr, size);
-            if (bufptr == NULL) {
-                return -1;
-            }
-        }
-        *p++ = c;
-        if (c == '\n') {
-            break;
-        }
-        c = fgetc(stream);
-    }
-
-    *p++ = '\0';
-    *lineptr = bufptr;
-    *n = size;
-
-    return p - bufptr - 1;
-}
 
 void initparametrs(int argc, char *argv[]){
     char *p;
@@ -68,8 +17,8 @@ void initparametrs(int argc, char *argv[]){
     long Klong;
     long dlong;
 
-    if(argc<5){
-        printf( "An Error Has Occurred");
+    if(argc<4){
+        printf( "An Error Has Occurred\n");
         exit(1);
     }
 
@@ -97,8 +46,8 @@ void initparametrs(int argc, char *argv[]){
         d = dlong;
     }
 
-    if(argc<6){
-        path= argv[4];
+    if(argc<5){
+        file = stdin;
     } else {
         long iterlong = strtol(argv[4], &p, 10);
         if ( *p != '\0' || iterlong > 1000 || iterlong < 1) {
@@ -106,30 +55,32 @@ void initparametrs(int argc, char *argv[]){
             exit(1);
         } else {
             iter = iterlong;
-            path= argv[5];
+            file = stdin;
         }
     }
 }
 
 void initK_centroid(){
-    FILE * fp;
     char * line;
     size_t len;
     int i,j;
 
-    fp = fopen(path, "r");
     line = NULL;
     len = 0;
-    if (fp == NULL)
+    if (file == NULL)
         exit(EXIT_FAILURE);
     Group_size = calloc(K,sizeof(double));
     K_centroid = calloc(K*d,sizeof(double));
+    data = calloc(N*d,sizeof(double));
+    places = calloc(N,sizeof(int ));
     for (i= 0; i < K; i++) {
-        if (getline(&line, &len, fp) != -1) {
+        places[i]=-1;
+        if (getline(&line, &len, file) != -1) {
             char *start = line;
             char *end;
             for (j = 0; j < d; j++) {
                 K_centroid[(i*d)+j] = strtod(start, &end);
+                data[(i * d) + j] = K_centroid[(i * d) + j];
                 if (start == end) {
                     fprintf(stderr, "An Error Has Occurred");
                     exit(1);
@@ -145,30 +96,24 @@ void initK_centroid(){
         }
         Group_size[i]=0;
     }
-
-    fclose(fp);
     if (line)
         free(line);
 }
 
 void init_data(){
-    FILE * fp;
     char *line,*start,*end;
     size_t len;
     int i,j,cnt;
-
-    fp = fopen(path, "r");
     line = NULL;
     len = 0;
-    if (fp == NULL)
+    if (file == NULL)
         exit(EXIT_FAILURE);
-    data = calloc(N*d,sizeof(double));
-    places = calloc(N,sizeof(int ));
+
 
     cnt=0;
-    for (i = 0; i < N; i++) {
+    for (i = K; i < N; i++) {
         places[i]=-1;
-        if (getline(&line, &len, fp) != -1) {
+        if (getline(&line, &len, file) != -1) {
             start = line;
             for (j = 0; j < d; j++) {
                 data[(i * d) + j] = strtod(start, &end);
@@ -184,15 +129,16 @@ void init_data(){
             cnt++;
         }
     }
-    N=cnt;
+    N=cnt+K;
 
-    fclose(fp);
     if (line)
         free(line);
 }
 
 void groupsMean(){
     int i,j;
+    free(Mean_vector);
+    Mean_vector = calloc(K*d,sizeof(double));
     for(i=0;i<K*d;i++) {
         Mean_vector[i]=0;
     }
@@ -282,7 +228,7 @@ int main(int argc, char *argv[]) {
     initparametrs(argc,argv);
     initK_centroid();
     init_data();
-    Mean_vector = calloc(K*d,sizeof(double));
+
 
     for(cnt=0;cnt<iter;cnt++){
         moveflag=0,deltasum=0;
@@ -306,5 +252,6 @@ int main(int argc, char *argv[]) {
         }
         printf("\n");
     }
+    free(Mean_vector);
     return 0;
 }
